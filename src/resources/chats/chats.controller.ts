7d414ab9898261e3ca778a90c2593401a7979b21
Jorge Soares <jorge.soares@linaopenx.com.br>
2025-10-26 19:28:32 -0300

    refactor: enhance ChatsController and ChatsService for improved message handling
    - Reformatted the handleSendMessage method in ChatsController for better readability.
    - Implemented OnModuleInit in ChatsService to load prompt from a markdown file.
    - Updated handleSaveMessage to utilize the loaded prompt and improved message handling logic.
    - Ensured consistent response structure in message handling.

--- src/resources/chats/chats.controller.ts
+++ src/resources/chats/chats.controller.ts
@@ -7,8 +7,12 @@ import { IMessage } from './chats.dto';
 export class ChatsController {
   constructor(private readonly chat: ChatsService) {}
   @Post('/message')
-  async handleSendMessage(@Req() req: Request, @Res() res: Response, @Body() payload: IMessage) {
+  async handleSendMessage(
+    @Req() req: Request,
+    @Res() res: Response,
+    @Body() payload: IMessage,
+  ) {
     const response = await this.chat.handleSaveMessage(payload);
-    return res.json({ send: true, ...response })
+    return res.json({ send: true, ...response });
   }
 }
--- src/resources/chats/chats.service.ts
+++ src/resources/chats/chats.service.ts
@@ -1,63 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma } from 'prisma/generated';
 import { OpenIAService } from 'src/services/openia.service';
 import { PrismaService } from 'src/services/prisma.service';
+import fs from 'fs';
+import path from 'path';
 
 @Injectable()
-export class ChatsService {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly openai: OpenIAService
-    ) {}
-    async getHistoric(chatId: string) {
-        const chat_messages = await this.prisma.message.findMany({
-            where: { chatId },
-            orderBy: { createdAt: 'desc' }
-        })
-        return chat_messages;
+export class ChatsService implements OnModuleInit {
+  model: string = 'gpt-4o-mini';
+  prompt: string;
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly openai: OpenIAService,
+  ) {}
+
+  onModuleInit() {
+    const prompt = fs.readFileSync(
+      path.join(process.cwd(), 'src/prompts/zero-shot-prompt-framework.md'),
+      'utf8',
+    );
+
+    this.prompt = prompt;
+  }
+
+  async getHistoric(chatId: string) {
+    const chat_messages = await this.prisma.message.findMany({
+      where: { chatId },
+      orderBy: { createdAt: 'desc' },
+    });
+    return chat_messages;
+  }
+
+  async handleSaveMessage(message: Prisma.MessageCreateInput) {
+    if (!message.chatId) {
+      /** Salvamos no inicio da conversa o prompt do nosso agente. */
+      const { chatId } = await this.prisma.message.create({
+        data: {
+          content: this.prompt,
+          role: 'system',
+        },
+      });
+
+      message.chatId = chatId;
     }
 
-    async handleSaveMessage(message: Prisma.MessageCreateInput) {
+    // Salvamos a mensagem do usuário
+    await this.prisma.message.create({ data: message });
 
-        if(!message.chatId) {
+    const messages = await this.getHistoric(message.chatId);
 
-            /** Salvamos no inicio da conversa o prompt do nosso agente. */
-            const { chatId } = await this.prisma.message.create({
-                data: {
-                    content: 'Você é o atendente da loja e deve dar informações sobre a loja',
-                    role: 'system'
-                }
-            })
+    const agente_response = await this.openai.client.chat.completions.create({
+      model: this.model,
+      temperature: 0.7,
+      messages: [...messages],
+    });
 
-            message.chatId = chatId;
+    const text = agente_response.choices[0].message.content || '';
 
-        }
+    const assistant_message: Prisma.MessageCreateInput = {
+      chatId: message.chatId,
+      content: text,
+      role: 'assistant',
+    };
 
-        // Salvamos a mensagem do usuário
-        await this.prisma.message.create({ data: message })
+    await this.prisma.message.create({ data: assistant_message });
 
-        const messages = await this.getHistoric(message.chatId);
-
-        const agente_response = await this.openai.client.chat.completions.create({
-            model: 'gpt-4o-mini',
-            messages: [
-                ...messages
-            ]
-        })
-
-        const text = agente_response.choices[0].message.content || "";
-
-        const assistant_message: Prisma.MessageCreateInput = {
-            chatId: message.chatId,
-            content: text,
-            role: 'assistant'
-        }
-
-        await this.prisma.message.create({ data: assistant_message })
-
-        return {
-            chatId: message.chatId,
-            content: text
-        };
-    }
+    return {
+      chatId: message.chatId,
+      content: text,
+    };
+  }
 }
